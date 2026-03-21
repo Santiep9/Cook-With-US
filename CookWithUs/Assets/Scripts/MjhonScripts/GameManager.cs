fb1dcf36e4ba85161f73a33b2525e2d18e7f9c69
Felix32323232 <felixramirochulvi2@gmail.com>
2026-03-21 21:01:19 +0100

    Mjohn Minigame

--- CookWithUs/Assets/Scripts/MjhonScripts/GameManager.cs
+++ CookWithUs/Assets/Scripts/MjhonScripts/GameManager.cs
@@ -1,9 +1,10 @@
+using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using System;
-using Unity.VisualScripting;
+using UnityEngine.Rendering.Universal.Internal;
 using UnityEngine.UI;
+
 
 public class GameManager : MonoBehaviour
 {
@@ -14,8 +15,29 @@ public class GameManager : MonoBehaviour
     [SerializeField] public Button Object5;
     [SerializeField] public Button Object6;
     [SerializeField] public Button Object7;
+    [SerializeField] public Button Object8;
+    [SerializeField] public Button Suspect1;
+    [SerializeField] public Button Suspect2;
+    [SerializeField] public Button Suspect3;
+    [SerializeField] public Button Suspect4;
+    [SerializeField] public Button Suspect5;
+    [SerializeField] public Button Suspect6;
+    [SerializeField] public Button Suspect7;
+    [SerializeField] public Button Suspect8;
+    [SerializeField] public Image Escritorio;
+    [SerializeField] public Image QuestionsBox;
     [SerializeField] public TextMeshProUGUI Text;
-    
+    [SerializeField] public TextMeshProUGUI QuestionText;
+    [SerializeField] public TextMeshProUGUI Contador;
+    [SerializeField] public TextMeshProUGUI John;
+    [SerializeField] public TextMeshProUGUI SuspectText;
+    [SerializeField] public TextMeshProUGUI BoxText;
+    [SerializeField] public TextMeshProUGUI FinalText;
+    [SerializeField] public TextMeshProUGUI BadFinalText;
+    int Object;
+    int contador;
+    int QueJohn;
+
     List<string> ObjectNames1 = new List<string>() { "Concha1", "Raqueta1", "TV1", "Sexo1" };
     List<string> ObjectNames2 = new List<string>() { "Concha2", "Raqueta2", "TV2", "Sexo2" };
     List<string> ObjectNames3 = new List<string>() { "Concha3", "Raqueta3", "TV3", "Sexo3" };
@@ -24,6 +46,8 @@ public class GameManager : MonoBehaviour
     List<string> ObjectNames6 = new List<string>() { "Concha6", "Raqueta6", "TV6", "Sexo6" };
     List<string> ObjectNames7 = new List<string>() { "Concha7", "Raqueta7", "TV7", "Sexo7" };
     List<string> ObjectNames8 = new List<string>() { "Concha8", "Raqueta8", "TV8", "Sexo8" };
+
+    List<string> Questions = new List<string>() { "¿Que sabes de Concha?", "¿Que sabes de Raqueta?", "¿Que sabes de TV?", "¿Que sabes de sexo?" };
 
 
     enum Player
@@ -44,11 +68,23 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         Text.enabled = false;
+        QuestionText.enabled = false;
+        BadFinalText.enabled = false;
+        FinalText.enabled = false;
         Listener(Object1, Object2, Object3, Object4, Object5, Object6, Object7);
+        PasoListener(Object8);
         ShowButton(Object1, Object2, Object3, Object4, Object5, Object6, Object7);
-
-
+        HidePaso(Object8);
+        HideSuspect1(Suspect1, Suspect2, Suspect3, Suspect4);
+        HideSuspect2(Suspect5, Suspect6, Suspect7, Suspect8);     
+        SuspectText.enabled = false;
     }
+    void Update()
+    {
+        Contador.text = "Preguntas restantes: " + contador.ToString() + " /24";
+        QueJohn = (int)current_player;
+        John.text = "John:" + (QueJohn + 1).ToString();
+    }   
 
     private void ObjectClicked(Button objectpass)
     {
@@ -57,30 +93,31 @@ public class GameManager : MonoBehaviour
         {
 
             case "Concha":
-                Debug.Log("Object 1 clicked");
-                TextDisplayer(0);
+                QuestionTextDisplayer(0);
                 HideButton(Object1, Object2, Object3, Object4, Object5, Object6, Object7);
+                Object = 0;
+                contador++;
                 break;
             case "Raqueta":
-                Debug.Log("Object 2 clicked");
-                TextDisplayer(1);
+                QuestionTextDisplayer(1);
                 HideButton(Object1, Object2, Object3, Object4, Object5, Object6, Object7);
+                Object = 1;
+                contador++;
                 break;
             case "TV":
-                Debug.Log("Object 3 clicked");
-                TextDisplayer(2);
+                QuestionTextDisplayer(2);
                 HideButton(Object1, Object2, Object3, Object4, Object5, Object6, Object7);
+                Object = 2;
+                contador++;
                 break;
             case "Sexo":
-                Debug.Log("Object 4 clicked");
-                TextDisplayer(3);
+                QuestionTextDisplayer(3);
                 HideButton(Object1, Object2, Object3, Object4, Object5, Object6, Object7);
-                break;
-            case "Paso":
-                Debug.Log("Object 5 clicked");
-                ShowButton(Object1, Object2, Object3, Object4, Object5, Object6, Object7);
+                Object = 3;
+                contador++;
                 break;
             case "Derecha":
+
                 if (current_player < Player.John8)
                 {
                     current_player = current_player + 1;
@@ -90,7 +127,74 @@ public class GameManager : MonoBehaviour
                 if (current_player > 0)
                 {
                     current_player = current_player - 1;
-                }   
+                }
+                break;
+            case "Paso":
+                QuestionText.enabled = false;
+                TextDisplayer(Object);
+                ShowPaso(Object8);
+                break;
+            case "Paso1":
+                if (contador >= 24)
+                {
+                    Escritorio.gameObject.SetActive(false);
+                    QuestionsBox.gameObject.SetActive(false);
+                    BoxText.enabled = false;
+                    Contador.enabled = false;
+                    John.enabled = false;
+                    Text.enabled = false;
+                    SuspectText.enabled = true;
+                    HidePaso(Object8);
+                    HidePaso1(Object5);
+                    ShowSuspect1(Suspect1, Suspect2, Suspect3, Suspect4);
+                    ShowSuspect2(Suspect5, Suspect6, Suspect7, Suspect8);
+                    SuspectListener(Suspect1, Suspect2, Suspect3, Suspect4, Suspect5, Suspect6, Suspect7, Suspect8);
+                    break;
+                }
+                ShowButton(Object1, Object2, Object3, Object4, Object5, Object6, Object7);
+                QuestionText.enabled = false;
+                HidePaso(Object8);
+                
+                break;
+            case "Suspect 1":
+                HideSuspect1(Suspect1, Suspect2, Suspect3, Suspect4);
+                HideSuspect2(Suspect5, Suspect6, Suspect7, Suspect8);
+                Win();
+                break;
+            case "Suspect 2":
+                HideSuspect1(Suspect1, Suspect2, Suspect3, Suspect4);
+                HideSuspect2(Suspect5, Suspect6, Suspect7, Suspect8);
+                Lose();
+                break;
+            case "Suspect 3":
+                HideSuspect1(Suspect1, Suspect2, Suspect3, Suspect4);
+                HideSuspect2(Suspect5, Suspect6, Suspect7, Suspect8);
+                Lose();
+                break;
+            case "Suspect 4":
+                HideSuspect1(Suspect1, Suspect2, Suspect3, Suspect4);
+                HideSuspect2(Suspect5, Suspect6, Suspect7, Suspect8);
+                Lose();
+                break;
+            case "Suspect 5":
+                HideSuspect1(Suspect1, Suspect2, Suspect3, Suspect4);
+                HideSuspect2(Suspect5, Suspect6, Suspect7, Suspect8);
+                Lose();
+                break;
+            case "Suspect 6":
+                HideSuspect1(Suspect1, Suspect2, Suspect3, Suspect4);
+                HideSuspect2(Suspect5, Suspect6, Suspect7, Suspect8);
+                Lose();
+                break;
+            case "Suspect 7":
+                HideSuspect1(Suspect1, Suspect2, Suspect3, Suspect4);
+                HideSuspect2(Suspect5, Suspect6, Suspect7, Suspect8);
+                Lose();
+                break;
+            case "Suspect 8":             
+                HideSuspect1(Suspect1, Suspect2, Suspect3, Suspect4);
+                HideSuspect2(Suspect5, Suspect6, Suspect7, Suspect8);
+                Lose();
                 break;
             default:
                 Debug.Log("Unknown object clicked");
@@ -99,19 +203,29 @@ public class GameManager : MonoBehaviour
 
     }
 
-    void Update()
+
+ 
+    void QuestionTextDisplayer(int button_n)
     {
+        if (Object1 != null) Object1.interactable = false;
+        if (Object2 != null) Object2.interactable = false;
+        if (Object3 != null) Object3.interactable = false;
+        if (Object4 != null) Object4.interactable = false;
 
-
-
+        
+        
+            QuestionText.enabled = true;
+            QuestionText.text = Questions[button_n];
+        
     }
-    // texto = jhon_list[buton_n]
     void TextDisplayer(int button_n)
     {
         if (Object1 != null) Object1.interactable = false;
         if (Object2 != null) Object2.interactable = false;
         if (Object3 != null) Object3.interactable = false;
         if (Object4 != null) Object4.interactable = false;
+
+
 
         switch (current_player)
         {
@@ -151,6 +265,14 @@ public class GameManager : MonoBehaviour
 
         }
     }
+   
+
+
+
+
+
+
+
     private void HideButton(Button btn, Button btn2, Button btn3, Button btn4, Button btn5, Button btn6, Button btn7)
     {
         if (btn == null) return;
@@ -160,6 +282,7 @@ public class GameManager : MonoBehaviour
         if (btn5 == null) return;
         if (btn6 == null) return;
         if (btn7 == null) return;
+   
         // ocultar botones principales y dejar el botón "Paso" visible
         btn.gameObject.SetActive(false);
         btn2.gameObject.SetActive(false);
@@ -170,7 +293,81 @@ public class GameManager : MonoBehaviour
         btn5.interactable = true;
         btn6.gameObject.SetActive(false);
         btn7.gameObject.SetActive(false);
-     
+
+    }
+    private void HideSuspect1(Button spc1, Button spc2 , Button spc3, Button spc4)
+    {
+        if (spc1 == null) return;
+        if (spc2 == null) return;
+        if (spc3 == null) return;
+        if (spc4 == null) return;
+        spc1.gameObject.SetActive(false);
+        spc2.gameObject.SetActive(false);
+        spc3.gameObject.SetActive(false);
+        spc4.gameObject.SetActive(false);
+    }
+    private void HideSuspect2(Button spc1, Button spc2, Button spc3, Button spc4)
+    {
+        if (spc1 == null) return;
+        if (spc2 == null) return;
+        if (spc3 == null) return;
+        if (spc4 == null) return;
+        spc1.gameObject.SetActive(false);
+        spc2.gameObject.SetActive(false);
+        spc3.gameObject.SetActive(false);
+        spc4.gameObject.SetActive(false);
+    }
+    private void ShowSuspect1(Button spc1, Button spc2, Button spc3, Button spc4)
+    {
+        if (spc1 == null) return;
+        if (spc2 == null) return;
+        if (spc3 == null) return;
+        if (spc4 == null) return;
+        spc1.gameObject.SetActive(true);
+        spc1.interactable = true;
+        spc2.gameObject.SetActive(true);
+        spc2.interactable = true;
+        spc3.gameObject.SetActive(true);
+        spc3.interactable = true;
+        spc4.gameObject.SetActive(true);
+        spc4.interactable = true;
+    }
+    private void ShowSuspect2(Button spc1, Button spc2, Button spc3, Button spc4)
+    {
+        if (spc1 == null) return;
+        if (spc2 == null) return;
+        if (spc3 == null) return;
+        if (spc4 == null) return;
+        spc1.gameObject.SetActive(true);
+        spc1.interactable = true;
+        spc2.gameObject.SetActive(true);
+        spc2.interactable = true;
+        spc3.gameObject.SetActive(true);
+        spc3.interactable = true;
+        spc4.gameObject.SetActive(true);
+        spc4.interactable = true;
+    }
+
+
+
+    private void ShowPaso(Button btn8)
+    {
+        if (btn8 == null) return;
+        btn8.gameObject.SetActive(true);
+        btn8.interactable = true;
+        PasoListener(btn8);
+    }
+    private void HidePaso(Button btn8)
+    {
+        if (btn8 == null) return;
+        btn8.gameObject.SetActive(false);
+        btn8.interactable = false;
+    }
+    private void HidePaso1(Button btn8)
+    {
+        if (btn8 == null) return;
+        btn8.gameObject.SetActive(false);
+        btn8.interactable = false;
     }
     private void ShowButton(Button btn, Button btn2, Button btn3, Button btn4, Button btn5, Button btn6, Button btn7)
     {
@@ -225,4 +422,56 @@ public class GameManager : MonoBehaviour
         seis.onClick.AddListener(() => ObjectClicked(seis));
         siete.onClick.AddListener(() => ObjectClicked(siete));
     }
+    private void PasoListener(Button paso)
+    {
+        if (paso == null) return;
+
+        paso.onClick.RemoveAllListeners();
+        paso.onClick.AddListener(() => ObjectClicked(paso));
+    }
+    private void SuspectListener(Button spc1, Button spc2, Button spc3, Button spc4, Button spc5, Button spc6, Button spc7, Button spc8)
+    {
+        if (spc1 == null) return;
+        if (spc2 == null) return;
+        if (spc3 == null) return;
+        if (spc4 == null) return;
+        if (spc5 == null) return;
+        if (spc6 == null) return;
+        if (spc7 == null) return;
+        if (spc8 == null) return;
+        spc1.onClick.RemoveAllListeners();
+        spc2.onClick.RemoveAllListeners();
+        spc3.onClick.RemoveAllListeners();
+        spc4.onClick.RemoveAllListeners();
+        spc5.onClick.RemoveAllListeners();
+        spc6.onClick.RemoveAllListeners();
+        spc7.onClick.RemoveAllListeners();
+        spc8.onClick.RemoveAllListeners();
+        spc1.onClick.AddListener(() => ObjectClicked(spc1));
+        spc2.onClick.AddListener(() => ObjectClicked(spc2));
+        spc3.onClick.AddListener(() => ObjectClicked(spc3));
+        spc4.onClick.AddListener(() => ObjectClicked(spc4));
+        spc5.onClick.AddListener(() => ObjectClicked(spc5));
+        spc6.onClick.AddListener(() => ObjectClicked(spc6));
+        spc7.onClick.AddListener(() => ObjectClicked(spc7));
+        spc8.onClick.AddListener(() => ObjectClicked(spc8));
+    }
+
+    void Win()
+    {
+        SuspectText.enabled = false;
+        FinalText.enabled = true;
+        HidePaso(Object8);
+        HidePaso1(Object5);
+    }
+    void Lose()
+    {
+        SuspectText.enabled = false;
+        BadFinalText.enabled = true;
+        HidePaso(Object8);
+        HidePaso1(Object5);
+    }
+
+
 }
+
