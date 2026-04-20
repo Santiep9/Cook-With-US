c0ebd3143baa9fa140e542a5b246f388a4c57e70
Santiep9 <155767000+Santiep9@users.noreply.github.com>
2026-04-20 19:37:04 +0200

    Boton de volver provisional
    Boton dentro de cada minijuego para volver

--- CookWithUs/Assets/Scripts/BtnVolverProvisional.cs
+++ CookWithUs/Assets/Scripts/BtnVolverProvisional.cs
@@ -1,9 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class BtnVolverProvisional : MonoBehaviour
 {
-    public void OnClick()
+    public void ChangeScene()
     {
         SceneManager.LoadScene("CityMap");
     }
