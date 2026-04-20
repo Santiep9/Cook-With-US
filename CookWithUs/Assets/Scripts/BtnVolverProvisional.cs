6b241c3c951dfb77254bf608400e5b9c99f06e9f
Santiep9 <155767000+Santiep9@users.noreply.github.com>
2026-04-20 19:29:02 +0200

    Cositas

---
+++ CookWithUs/Assets/Scripts/BtnVolverProvisional.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BtnVolverProvisional : MonoBehaviour
+{
+    public void OnClick()
+    {
+        SceneManager.LoadScene("CityMap");
+    }
+}
